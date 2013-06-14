3c01671093fa3726eb321b306564d3b43f45f60f
IZonker <armen.he@gmail.com>
2013-06-14 18:57:03 +0400

    Sorting

--- DSA/Algorithms/Sorting/DutchNationalFlagProblem.cs
+++ DSA/Algorithms/Sorting/DutchNationalFlagProblem.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace DSA.Algorithms
+namespace DSA.Algorithms.Sorting
 {
     public static partial class Sorting
     {
--- DSA/Algorithms/Sorting/InsertionSort.cs
+++ DSA/Algorithms/Sorting/InsertionSort.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace DSA.Algorithms
+namespace DSA.Algorithms.Sorting
 {
 
     public static partial class Sorting
