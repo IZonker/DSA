78fd895cb821d2a9594d1329ead5f83635b572ce
IZonker <armen.he@gmail.com>
2013-06-14 16:50:16 +0400

    Init solution

---
+++ DSA.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSA.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
